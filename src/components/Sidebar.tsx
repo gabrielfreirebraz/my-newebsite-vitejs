5cf86b614639a9f11c2d711165867c774368bb10
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-06-18 02:13:56 -0300

    feat: avatar ok

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,8 +6,13 @@ export const Sidebar = () => {
     <>
       <aside className={styles.sidebar}>
 
-        <img src="https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=50" />
-        {/* <img src="https://github.com/gabrielfreirebraz.png" /> */}
+        <img className={styles.profile} src="https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=50" />
+        
+        <div className={styles.avatar}>
+          <img src="https://github.com/gabrielfreirebraz.png" />
+        </div> 
+
+        <strong>Gabriel Freire Braz</strong>
       </aside>        
     </>
   );
