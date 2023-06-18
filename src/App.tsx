caa9257e76ac6224514f5859eabf8794948d7768
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-06-18 00:10:37 -0300

    feat: add aside

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 
 import './global.css'
 import {Header} from './components/Header';
+import {Sidebar} from './components/Sidebar';
 
 function App() {
 
   return (
     <>
         <Header></Header>
+        <Sidebar></Sidebar>
     </>
   )
 }
